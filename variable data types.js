431b9d1f7e04e497f26b71c4c65b7d341f154355
mahmudul-hasan <mhmahmudul1211@gmail.com>
2024-08-31 23:25:52 +0600

    comment added

--- variable data types.js
+++ variable data types.js
@@ -18,3 +18,16 @@ console.log(age)
 console.log(typeof age)
 console.log(typeof passed)
 console.log(typeof name)
+
+// things to be maintain before declare a variable 
+// 1. can not start with a number 
+// 2. no gap between two word 
+// 3.keyword cannot be use 
+// 4.no quote
+// 5.case sensitive
+
+// how to write long variable name 
+var my_name = "hasan"; [snake case]
+var myName = "hasan";  [camel case]
+var MyName = "hasan"; [pascal case]
+var my_name = "hasan";
