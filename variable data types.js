62ec783f127b2b532fec470debbc8ee38af87515
mahmudul-hasan <mhmahmudul1211@gmail.com>
2024-08-31 23:17:17 +0600

    first commit

---
+++ variable data types.js
@@ -0,0 +1,20 @@
+//number type
+var age = 23;
+console.log(age)
+
+//string
+var name = 'mahmudul hasan'
+console.log(name)
+
+// boolean
+var passed = true;
+console.log(passed)
+
+// change value 
+age = 26;
+console.log(age)
+
+// type of(checking the type of value)
+console.log(typeof age)
+console.log(typeof passed)
+console.log(typeof name)
